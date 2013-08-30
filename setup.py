60cc17b08c6381509466061b7c4d71311394deae
Michael JasonSmith <mpj17@onlinegroups.net>
2013-08-30 13:49:25 -0700

    Adding a copyright and license.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
 import os
 from setuptools import setup, find_packages
 from version import get_version
@@ -11,27 +11,37 @@ setup(name='gs.site.member',
     long_description=open("README.txt").read() + "\n" +
                       open(os.path.join("docs", "HISTORY.txt")).read(),
     classifiers=[
-      "Development Status :: 4 - Beta",
-      "Environment :: Web Environment",
-      "Framework :: Zope2",
-      "Intended Audience :: Developers",
-      "License :: Other/Proprietary License",
-      "Natural Language :: English",
-      "Operating System :: POSIX :: Linux"
-      "Programming Language :: Python",
-      "Topic :: Software Development :: Libraries :: Python Modules",
+        'Development Status :: 5 - Production/Stable',
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: Zope Public License',
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux"
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
       ],
-    keywords='site groupserver name',
+    keywords='site groupserver member statistics join leave',
     author='Michael JasonSmith',
     author_email='mpj17@onlinegroups.net',
     url='http://groupserver.org/',
-    license='other',
+    license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.site',],
+    namespace_packages=['gs', 'gs.site', ],
     include_package_data=True,
     zip_safe=True,
     install_requires=[
         'setuptools',
+        'pytz',
+        'zope.cachedescriptors',
+        'zope.component',
+        'zope.contentprovider',
+        'zope.event',
+        'zope.interface',
+        'zope.pagetemplate'
+        'zope.publisher',
+        'zope.schema',
+        'Zope2',
         'gs.group.member.base',
         'gs.group.member.join',
         'gs.group.member.leave',
@@ -39,9 +49,7 @@ setup(name='gs.site.member',
         'Products.CustomUserFolder',
         'Products.GSAuditTrail',
         'Products.GSGroupMember',
-        # -*- Extra requirements: -*-
     ],
     entry_points="""
     # -*- Entry points: -*-
     """,)
-
