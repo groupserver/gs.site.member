d4ea98fd4833a39fdc265f8f1d15bdd67b5fad4a
Michael JasonSmith <mpj17@onlinegroups.net>
2013-09-23 16:55:43 -0700

    Minor PEP-8 cleanups.

--- gs/site/member/sitemembershipvocabulary.py
+++ gs/site/member/sitemembershipvocabulary.py
@@ -1,14 +1,28 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2013 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
 from operator import attrgetter
-from zope.interface import implements, providedBy
+from zope.cachedescriptors.property import Lazy
 from zope.component import createObject
+from zope.interface import implements, providedBy
+from zope.interface.common.mapping import IEnumerableMapping
 from zope.schema.vocabulary import SimpleTerm
-from zope.schema.interfaces import IVocabulary,\
-  IVocabularyTokenized, ITitledTokenizedTerm
-from zope.interface.common.mapping import IEnumerableMapping 
-import AccessControl
+from zope.schema.interfaces import IVocabulary, IVocabularyTokenized, \
+    ITitledTokenizedTerm
 
 SITE_FOLDER_TYPES = ('Folder', 'Folder (Ordered)')
+
 
 class SiteMembership(object):
     implements(IVocabulary, IVocabularyTokenized)
@@ -16,8 +30,7 @@ class SiteMembership(object):
 
     def __init__(self, user):
         self.context = user
-        self.__sites = None
-       
+
     def __iter__(self):
         """See zope.schema.interfaces.IIterableVocabulary"""
         retval = [SimpleTerm(s, s.id, s.name)
@@ -42,31 +55,30 @@ class SiteMembership(object):
     def getTerm(self, value):
         """See zope.schema.interfaces.IBaseVocabulary"""
         return self.getTermByToken(value)
-        
+
     def getTermByToken(self, token):
         """See zope.schema.interfaces.IVocabularyTokenized"""
         for s in self.sites:
             if s.id == token:
-                retval = SimpleTerm(s, s.id, g.name)
+                retval = SimpleTerm(s, s.id, s.name)
                 assert retval
                 assert ITitledTokenizedTerm in providedBy(retval)
                 assert retval.token == retval.value
                 return retval
-        raise LookupError, token
+        raise LookupError(token)
 
-    @property
+    @Lazy
     def sites(self):
         assert self.context
-        if self.__sites == None:
-            content = self.context.Content
-            memberships = ['_'.join(m.split('_')[:-1])
-                           for m in self.context.getGroups()]
-            siteMembershipIds = [m for m in memberships 
-                        if m in content.objectIds(SITE_FOLDER_TYPES)]
-            siteInstances = [getattr(content,s) 
-                             for s in siteMembershipIds]
-            self.__sites = [createObject('groupserver.SiteInfo', s)
-                            for s in siteInstances]
-        assert type(self.__sites) == list
-        return self.__sites
-
+        content = self.context.Content
+        memberships = ['_'.join(m.split('_')[:-1])
+                       for m in self.context.getGroups()]
+        siteMembershipIds = [m for m in memberships
+                            if m in content.objectIds(SITE_FOLDER_TYPES)]
+        retval = []
+        for s in siteMembershipIds:
+            site = getattr(content, s)
+            siteInfo = createObject('groupserver.SiteInfo', site)
+            retval.append(siteInfo)
+        assert type(retval) == list
+        return retval
