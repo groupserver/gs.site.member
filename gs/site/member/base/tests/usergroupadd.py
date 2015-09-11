0531def8fe2db33bfa6d28ab3eca000322a2844b
Michael JasonSmith <mpj17@onlinegroups.net>
2015-09-11 11:40:26 -0700

    Making a start on the member_added unit tests

--- gs/site/member/base/tests/usergroupadd.py
+++ gs/site/member/base/tests/usergroupadd.py
@@ -14,8 +14,27 @@
 ############################################################################
 from __future__ import absolute_import, unicode_literals
 from unittest import TestCase
-# from mock import patch
+from mock import patch, MagicMock
+from gs.site.member.base.usergroupadd import (
+    member_added, JOIN_SITE_MEMBER, )
 
 
 class TestAdd(TestCase):
     '''Test joining a group'''
+
+    def setUp(self):
+        self.mockEvent = MagicMock()
+        ui = self.mockEvent.memberInfo
+        ui.id = b'example_user'
+        gi = self.mockEvent.groupInfo
+        si = gi.siteInfo
+        si.id = b'example_site'
+
+    @patch('gs.site.member.base.usergroupadd.user_member_of_site')
+    @patch('gs.site.member.base.usergroupadd.SiteMemberAuditor')
+    def test_already_a_member(self, Mock_SMA, mock_umos):
+        mock_umos.return_value = True
+
+        member_added(MagicMock(), self.mockEvent)
+
+        Mock_SMA().info.assert_called_once_with(JOIN_SITE_MEMBER)
